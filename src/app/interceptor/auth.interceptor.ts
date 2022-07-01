b58972559e6c4fb8cf244db505041664aa0fd445
hacker1998-thosiba <sowcheikh086@gmail.com>
2022-07-01 12:54:36 +0000

    Creation des intercepteurs

---
+++ src/app/interceptor/auth.interceptor.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../service/authentication.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authenticationService: AuthenticationService) { }
+
+  intercept(httpRequest: HttpRequest<any>, httpHandler: HttpHandler): Observable<HttpEvent<any>> {
+
+    if (HttpRequest.url.includes(`${this.authenticationService.host}/user/login`)) {
+      return httpHandler.handle(httpRequest);
+    }
+    if (HttpRequest.url.includes(`${this.authenticationService.host}/user/register`)) {
+      return httpHandler.handle(httpRequest);
+    }
+    if (HttpRequest.url.includes(`${this.authenticationService.host}/user/reset-password`)) {
+      return httpHandler.handle(httpRequest);
+    }
+    this.authenticationService.loadToken();
+
+  }
+}
