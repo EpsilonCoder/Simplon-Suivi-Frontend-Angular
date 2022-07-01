919ae456a49b9430199ba253f74f6a7e7edfdebc
hacker1998-thosiba <sowcheikh086@gmail.com>
2022-07-01 16:42:04 +0000

    Implementation des notifications

--- src/app/guard/authentication.guard.ts
+++ src/app/guard/authentication.guard.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { NotificationType } from '../enum/notification-type.enum';
 import { AuthenticationService } from '../service/authentication.service';
+import { NotificationService } from '../service/notification.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
 
-  constructor(private authenticationService: AuthenticationService, private router: Router) { }
+  constructor(private notificationService: NotificationService, private authenticationService: AuthenticationService, private router: Router) { }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     return this.isUserLoggedIn();
   }
@@ -18,7 +20,9 @@ export class AuthenticationGuard implements CanActivate {
     if (this.authenticationService.isUserLoggedIn()) {
       return true;
     }
+    //redirection vers la page d'authentification
     this.router.navigate(['/login']);
+    this.notificationService.notify(NotificationType.ERROR, `Simplon :joystick: vous devez etre connecté pour avoir acces a ce contenue`.toUpperCase());
     //send notification to user
     return false
   }
