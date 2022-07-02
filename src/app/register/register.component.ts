790c461d604b7ff1617ee58e4239dc2fa20e1188
hacker1998-thosiba <sowcheikh086@gmail.com>
2022-07-02 19:32:58 +0000

    implementation de la connexion et inscription

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user';
+import { NotificationType } from '../enum/notification-type.enum';
 
+import { HttpErrorResponse } from '@angular/common/http';
+import { NotificationService } from '../service/notification.service';
+import { AuthenticationService } from '../service/authentication.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  showLoading: boolean | undefined;
+  private subscriptions: Subscription[] = [];
+
+  constructor(private router: Router, private authenticationService: AuthenticationService,
+    private notificationService: NotificationService) { }
 
   ngOnInit(): void {
+    if (this.authenticationService.isUserLoggedIn()) {
+      this.router.navigateByUrl('/user/management');
+    }
   }
 
+  public onRegister(user: User): void {
+    console.log(user);
+    this.showLoading = true;
+    this.subscriptions.push(
+      this.authenticationService.register(user).subscribe(
+        (response: User) => {
+          this.showLoading = false;
+          this.sendNotification(NotificationType.SUCCESS, `Un nouveau compte vient d'etre créer pour ${response.firstName}.
+                                      Veuillez consulter votre Email pour récuperer votre mot de passe d'acces`);
+        },
+        (errorResponse: HttpErrorResponse) => {
+          this.sendNotification(NotificationType.ERROR, errorResponse.error.message);
+          this.showLoading = false;
+        }
+      )
+    );
+  }
+
+  private sendNotification(notificationType: NotificationType, message: string): void {
+    if (message) {
+      this.notificationService.notify(notificationType, message);
+    } else {
+      this.notificationService.notify(notificationType, 'Une erreur c est produit veuillez réessayer , s il vou plait');
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
 }
